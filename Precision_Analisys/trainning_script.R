fa8b166149819d60733d5e8ee7a443b1330e13db
Vinicius Couto Espindola <vinicius.c.e@hotmail.com>
2019-08-28 18:01:16 -0300

    added some histograms

--- Precision_Analisys/trainning_script.R
+++ Precision_Analisys/trainning_script.R
@@ -1,5 +1,6 @@
 library(pacman)
-p_load(raster, ncdf4, maptools)
+p_load(raster, ncdf4, maptools, rgdal, ggmap, ggplot2, reshape2, stringr, mapdata, mapview)
+
 
 early_files <- list.files('data/early_run',pattern='*.nc',full.names=TRUE)
 early_stack <- raster::stack(early_files)
@@ -28,12 +29,50 @@ image(lon, lat, as.matrix(flip(final_stack[[1]],'y')))
 data(wrld_simpl)
 plot(wrld_simpl,add=TRUE,phc=3)
 
-early_diff <- abs(early_stack[[1]]-final_stack[[1]])
+early_diff <- early_stack[[1]]-final_stack[[1]]
 image(lon, lat, as.matrix(flip(early_diff,'y')))
 data(wrld_simpl)
 plot(wrld_simpl,add=TRUE,phc=4)
 
-late_diff <- abs(late_stack[[1]]-final_stack[[1]])
+late_diff <- late_stack[[1]]-final_stack[[1]]
 image(lon, lat, as.matrix(flip(late_diff,'y')))
 data(wrld_simpl)
 plot(wrld_simpl,add=TRUE,phc=5)
+
+s <- 1
+e <- 30
+diff_sum = 0
+for (i in s:e){
+  diff_sum = (diff_sum + (early_stack[[i]] - final_stack[[i]]))**2  
+} 
+
+test <- early_stack[[1]]
+for (i in 1:length(lat)){
+  for (j in 1:length(lon)){
+    test[i,j] = 1
+  }
+} 
+
+par(mfrow=c(1,1))
+
+ggplot(data =  as.matrix(flip(late_diff,'y')), mapping = aes(x = longitude, y = latitude)) + 
+geom_point(aes(col = median_price, size = transactions)) +
+geom_text(aes(label = city), size = 2, nudge_y = 0.01) +
+scale_color_distiller(palette = "YlOrRd", direction = 1)
+
+# Plot graph indicating the percentage of a certain error in the map 
+hist_data <- hist(early_diff[early_diff!=0], breaks = 2000)
+hist_data$counts <- (hist_data$counts/length(early_diff[[1]]))*100
+hist_data$breaks <- hist_data$breaks[-1]
+x <- hist_data$breaks[hist_data$counts>0.1]
+y <- hist_data$counts[hist_data$counts>0.1]
+plot(x, y, type='s')
+
+# Plot graph indicating the percentage of positive erros in the map
+hist_data <- hist(early_diff[early_diff>0], breaks = 2000)
+hist_data$counts <- (hist_data$counts/length(early_diff[[1]]))*100
+hist_data$breaks <- hist_data$breaks[-1]
+x <- hist_data$breaks[hist_data$counts>0.1]
+y <- hist_data$counts[hist_data$counts>0.1]
+plot(x, y, type='s')
+
